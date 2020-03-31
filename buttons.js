06620033b32f1ee4e74c3c572d1f8080d4127bab
PauliusLat <56875176+PauliusLat@users.noreply.github.com>
2020-03-31 16:51:23 +0300

    click number

---
+++ buttons.js
@@ -0,0 +1,20 @@
+"use strict";
+
+let HTML = ``;
+for(let i = 0; i<4; i++){
+HTML+= '<div class="btn" data-nr="0">Paspaudimai: </div>';
+}
+
+document.querySelector('body').innerHTML = HTML;
+
+const butonsai = document.querySelectorAll('.btn');
+for (let i = 0; i < butonsai.length; i++) {
+    butonsai[i].addEventListener('click', increment);
+}
+function increment(event) {
+    const button = event.target;
+    let nr = parseInt(button.dataset.nr);
+    button.dataset.nr = nr+1;
+    button.innerText = button.dataset.nr;
+    
+}
